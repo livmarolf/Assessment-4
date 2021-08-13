af32dead3f68aea32b0f3d758ee8043e187557c1
Olivia Marolf <livmarolf@gmail.com>
2021-08-13 10:13:13 -0600

    adding python segment of the assessment

--- process.py
+++ process.py
@@ -1,11 +1,12 @@
+#here is where events will be tracked every time the software runs
 log_file = open("um-server-01.txt")
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
+def sales_reports(log_file): #declaring a function called sales_reports with an argument of log_file
+    for line in log_file:#for each line in the file do the following ...
+        line = line.rstrip()#.rstrip will remove any spaces at the end of the string
+        day = line[0:3]#only allows up to three numbers after the string?
+        if day == "Mon":#printing only tuesday and changed to monday
             print(line)
 
 
